55b3dc1d1077aaecc62b4fde58e63aab7aec4eb4
susmita <susmita.patil@globant.com>
2024-02-06 14:57:14 +0530

    [update-eat-n-split]:Updated project to resolve bug

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -80,6 +80,7 @@ export default function App() {
           <FormSplitBill
             selectedFriend={selectedFriend}
             handleSplitBill={handleSplitBill}
+            key={selectedFriend.id}
           />
         )}
       </div>
