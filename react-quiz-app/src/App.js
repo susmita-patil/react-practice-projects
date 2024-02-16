3c1d25f107dbc4506e5e97ce2d609a0ba2cec04a
susmita <susmita.patil@globant.com>
2024-02-16 21:47:22 +0530

    new react quiz app project using react useReducer hook

---
+++ react-quiz-app/src/App.js
@@ -0,0 +1,137 @@
+import "./App.css";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Loader from "./Components/Loader";
+import Error from "./Components/Error";
+import StartScreen from "./Components/StartScreen";
+import Question from "./Components/Question";
+import NextQuestion from "./Components/NextQuestion";
+import Progress from "./Components/Progress";
+import FinishScreen from "./Components/FinishScreen";
+
+const initialState = {
+  questions: [],
+  // loading/error/ready/active/finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index, answer, points, highScore } = state;
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (accumulator, curQues) => accumulator + curQues.points,
+    0
+  );
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (e) {
+        console.log(e);
+        dispatch({ type: "dataFailed" });
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              numQuestions={numQuestions}
+              index={index}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextQuestion
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highScore={highScore}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-quiz-app/src/Components/FinishScreen.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function FinishScreen({ maxPossiblePoints, points, highScore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "";
+  if (percentage >= 80 && percentage < 100) emoji = "";
+  if (percentage >= 50 && percentage < 80) emoji = "";
+  if (percentage >= 0 && percentage < 50) emoji = "";
+  if (percentage === 0) emoji = "";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}) %
+      </p>
+      <p className="highscore">(Highscore: {highScore} points)</p>
+    </>
+  );
+}
